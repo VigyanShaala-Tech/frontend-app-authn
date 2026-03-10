f4e086a58d7f3d097ce6df8c938531643706bc42
Sonu-TitanEd <SonuK@TitanEd.com>
2026-03-10 13:34:25 +0530

    fix autn left message

--- src/base-container/components/custom-layout/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as CustomLargeLayout } from './CustomLargeLayout';
-export { default as CustomMediumLayout } from './CustomMediumLayout';
-export { default as CustomSmallLayout } from './CustomSmallLayout';
